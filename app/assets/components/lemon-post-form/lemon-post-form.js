5de49295b0247e425bb1442d8d1b691fce9d35af
Daniel Smith <jellymann@gmail.com>
2016-03-12 21:54:15 +0200

    style new/edit post forms

---
+++ app/assets/components/lemon-post-form/lemon-post-form.js
@@ -0,0 +1,22 @@
+Polymer({
+  is: "lemon-post-form",
+
+  listeners: {
+    'lemon_submit_post.click': 'submitForm'
+  },
+
+  ready: function() {
+    this.form = this.querySelector('form');
+    this._post_title = this.form.querySelector('#post_title');
+    this._post_body = this.form.querySelector('#post_body');
+
+    this.$.lemon_post_title.value = this._post_title.value;
+    this.$.lemon_post_body.value = this._post_body.value;
+  },
+
+  submitForm: function() {
+    this._post_title.value = this.$.lemon_post_title.value;
+    this._post_body.value = this.$.lemon_post_body.value;
+    this.form.submit();
+  }
+});
