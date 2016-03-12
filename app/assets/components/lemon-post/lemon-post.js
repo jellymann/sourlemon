7ca13e9220257d3f826e8623346b8d5461414887
Daniel Smith <jellymann@gmail.com>
2016-03-12 15:24:43 +0200

    use lemon-post component for index

--- app/assets/components/lemon-post/lemon-post.js
+++ app/assets/components/lemon-post/lemon-post.js
@@ -2,7 +2,6 @@ Polymer({
   is: "lemon-post",
   properties: {
     title: String,
-    body: String,
     url: String,
     summary: {
       type: Boolean,
@@ -10,7 +9,30 @@ Polymer({
     }
   },
 
+  listeners: {
+    'tap': 'onTap'
+  },
+
   ready: function() {
     this.$.titleTag.href = this.url;
+    if (this.summary) {
+      this.$.postMaterial.animated = true;
+      this.$.postMaterial.elevation = 1;
+    }
+  },
+
+  onTap: function() {
+    if (!this.summary) return;
+    window.location.href = this.url;
+  },
+
+  onMouseover: function() {
+    if (!this.summary) return;
+    this.$.postMaterial.elevation = 3;
+  },
+
+  onMouseout: function() {
+    if (!this.summary) return;
+    this.$.postMaterial.elevation = 1;
   }
 });
