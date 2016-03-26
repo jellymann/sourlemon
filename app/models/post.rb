71fbbea25c4616774397ebfae84fc8d1ce973628
Daniel Smith <jellymann@gmail.com>
2016-03-26 14:57:09 +0200

    refactor post summarry trimming into own class

--- app/models/post.rb
+++ app/models/post.rb
@@ -42,10 +42,7 @@ class Post < ApplicationRecord
   end
 
   def generate_summary_html
-    # puts "\n" + "/"*50
-    # puts summary_trim_script
-    # puts "\n" + "/"*50
-    phantomjs.inline(summary_trim_script).strip
+    PostSummaryTrimmer.new(self).perform
   end
 
   private
@@ -56,121 +53,5 @@ class Post < ApplicationRecord
 
   def markdown
     @_markdown ||= Redcarpet::Markdown.new(LemonHTMLRenderer, fenced_code_blocks: true)
-  end
-
-  def summary_markdown
-    @_markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
-  end
-
-  def phantomjs
-    @_phantomjs ||= Phantomjs.new()
-  end
-
-  def html_for_summary
-    summary_markdown.render(self.body)
-  end
-
-  def html_wrapper_for_summary_trim
-    """
-    <div class=\"content\" id=\"content\" style=\"opacity: 1;\">
-      <div class=\"post post-summary\" id=\"postContainer\">
-        <div class=\"pre-body\">
-          -
-        </div>
-        <div class=\"body\" id=\"postBody\">
-          <h1>#{title}</h1>
-        </div>
-      </div>
-    </div>
-    """
-  end
-
-  def js_try_catch
-    """
-    try {
-      #{yield}
-    } catch (error) {
-      console.log('*** SOMETHING WENT WRONG ***');
-      console.log(error.stack);
-      alert('DONE');
-    }
-    """
-  end
-
-  def phantom_page_evaluate
-    """
-    try {
-      var page = require('webpage').create();
-      page.viewportSize = { width: 1920, height: 1080 };
-      page.onConsoleMessage = function(msg) {
-        console.log(msg);
-      };
-      page.onAlert = function(msg) {
-        if (msg === 'DONE') phantom.exit();
-      };
-      page.open('about:blank', function(status) {
-        page.evaluate(function() {
-          #{ js_try_catch { yield } }
-        });
-      });
-    } catch (error) {
-      console.log('*** SOMETHING WENT WRONG ***');
-      console.log(error.stack);
-      phantom.exit();
-    }
-    """
-  end
-
-  def summary_trim_boilerplate
-    """
-    document.head.innerHTML += '<style>' +
-      #{::Sprockets::Railtie.build_environment(Rails.application)["application-inline.css"].to_s.chomp.to_json} +
-      #{::Sprockets::Railtie.build_environment(Rails.application)["posts-index-inline.css"].to_s.chomp.to_json} +
-      #{::Sprockets::Railtie.build_environment(Rails.application)["application.css"].to_s.chomp.to_json} +
-    '</style>';
-    document.getElementsByTagName('html')[0].classList.add('css-loaded');
-    #{::Sprockets::Railtie.build_environment(Rails.application)["webfontloader/webfontloader.js"].to_s}
-    WebFont.load({
-      google: { families: [
-        'Roboto:400,300,300italic,400italic,500,500italic,700,700italic',
-        'Roboto+Mono:400,700'
-      ] },
-      active: function() {
-        #{ js_try_catch { yield } }
-      }
-    });
-    """
-  end
-
-  def summary_trim_script
-    phantom_page_evaluate do
-      summary_trim_boilerplate do
-        """
-        document.body.innerHTML += #{html_wrapper_for_summary_trim.to_json};
-
-        var wrapper = document.getElementById('postBody');
-        var postContainer = document.getElementById('postContainer');
-        var maxHeight = document.defaultView.getComputedStyle(postContainer, null).getPropertyValue('max-height');
-        maxHeight = parseInt(maxHeight.replace(/(\\d+)px/, '$&'), 10);
-
-        var fullBody = document.createElement('div');
-        fullBody.innerHTML = #{html_for_summary.to_json};
-
-        var lastHeight = 0;
-        while (fullBody.children.length > 0) {
-          wrapper.appendChild(fullBody.children[0]);
-          if (postContainer.clientHeight === lastHeight) {
-            wrapper.children[wrapper.children.length - 1].remove();
-            break;
-          }
-          if (postContainer.clientHeight >= maxHeight) break;
-          lastHeight = postContainer.clientHeight;
-        }
-        wrapper.getElementsByTagName('h1')[0].remove();
-        console.log(wrapper.innerHTML);
-        alert('DONE');
-        """
-      end
-    end
   end
 end
