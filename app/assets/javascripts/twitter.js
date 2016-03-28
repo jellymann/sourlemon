6bfccedb1285d2e79f1b8c23c156351b3f2abe99
Daniel Smith <jellymann@gmail.com>
2016-03-28 09:27:15 +0200

    add facebook button

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -8,3 +8,16 @@
     fjs.parentNode.insertBefore(js, fjs);
   }
 }(document, 'script', 'twitter-wjs');
+
+document.addEventListener('turbolinks:load', function() {
+  var title = document.getElementsByTagName('h1')[0].innerText;
+  var button, buttons = document.getElementsByClassName('twitter-share-button');
+  for (var i = 0; i < buttons; i++) {
+    console.log("Setting things on twitter button");
+    button = buttons[i];
+    if (!button.dataset['data-url']) button[i].setAttribute('data-url', document.location.href);
+    if (!button.dataset['data-text']) button[i].setAttribute('data-text', title);
+  }
+  console.log('loading twttr widgets');
+  twttr.widgets.load();
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( application-load.js )
-Rails.application.config.assets.precompile += %w( twitter.js )
+Rails.application.config.assets.precompile += %w( social_media.js )
